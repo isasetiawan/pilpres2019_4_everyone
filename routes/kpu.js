6a4372e7f0eeab3602cb133a70fb3e87d80b33b3
isasetiawan <azaqokay@gmail.com>
2019-04-28 21:42:11 +0700

    beutify the response

--- routes/kpu.js
+++ routes/kpu.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios')
 const https = require('https')
+const provinces = require('../consts/provinces')
 
 const instance = axios.create({
   httpsAgent: new https.Agent({  
@@ -13,7 +14,17 @@ const instance = axios.create({
 router.get('/', async function(req, res, next) {
   try {
     let {data} = await instance.get("https://pemilu2019.kpu.go.id/static/json/hhcw/ppwp.json");
-    return res.json(data)   
+    let {table, chart, ts} = data
+    let keys = Object.keys(table)
+
+    let persebaran = {}
+
+    for(const key of keys){
+      let name = provinces[key]
+      persebaran[name] = table[key]
+    }
+    return res.json({chart,time:ts,persebaran}) 
+
   } catch (error) {
     console.log(error.Error)
     return res.json({msg:"error"})
