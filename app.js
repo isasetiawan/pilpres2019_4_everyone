5f42fc89a3fe5fcb61651ef2a554fd4b0800b550
isasetiawan <azaqokay@gmail.com>
2019-04-28 21:41:19 +0700

    config cors

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var kpuRouter = require('./routes/kpu');
 
 var app = express();
+
+var whitelist = ['http://localhost:3000', 'https://realcount.id']
+var corsOptions = {
+  origin: 'https://realcount.id',
+  credential:true
+}
+
+app.use(cors(corsOptions))
 
 app.use(logger('dev'));
 app.use(express.json());
