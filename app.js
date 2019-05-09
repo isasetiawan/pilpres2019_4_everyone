02d0999b3cc9205775b5c92499d5ffbda1423166
isasetiawan <azaqokay@gmail.com>
2019-05-09 12:03:17 +0700

    require cron

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
+require('./cron/kpu')
 
 var indexRouter = require('./routes/index');
 var kpuRouter = require('./routes/kpu');
